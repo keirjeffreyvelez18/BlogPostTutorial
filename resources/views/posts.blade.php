c1c4d0e0109fa39535ffd6b2926a7cc6e91ac620
keirjeffreyvelez18 <32511317+keirjeffreyvelez18@users.noreply.github.com>
2022-07-09 07:37:32 +0800

    Video 11

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="posts/first">My First Blog</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, soluta. Necessitatibus ullam non aspernatur quaerat sequi blanditiis, culpa molestiae adipisci ducimus magnam facere iusto, iure quas nobis neque sint consectetur!</p>
+    </article>
+    <article>
+        <h1><a href="posts/second">My Second Blog</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, soluta. Necessitatibus ullam non aspernatur quaerat sequi blanditiis, culpa molestiae adipisci ducimus magnam facere iusto, iure quas nobis neque sint consectetur!</p>
+    </article>
+    <article>
+        <h1><a href="posts/third">My Third Blog</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, soluta. Necessitatibus ullam non aspernatur quaerat sequi blanditiis, culpa molestiae adipisci ducimus magnam facere iusto, iure quas nobis neque sint consectetur!</p>
+    </article>
+</body>
+</html>
